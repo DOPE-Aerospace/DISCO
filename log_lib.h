2ae2493ec35d06c352b1cef6e6db150d0651e4b6
ThePirate42 <git@thepirate42.org>
2023-04-25 20:20:59 +0200

    Small style channge, moved include guard before includes.

--- log_lib.h
+++ log_lib.h
@@ -1,11 +1,11 @@
+#ifndef log_lib
+#define log_lib
+
 #include <Timer.h>
 #include <SPI.h>  //Serial Peripheral Interface: Used for communication between SD card and Arduino
 #include <SD.h>
 #include "status_handler.h"
 #include "config.h"
-
-#ifndef log_lib
-#define log_lib
 
 void SD_Connect();
 void record_event(String, String&, Timer&);
--- status_handler.h
+++ status_handler.h
@@ -1,8 +1,8 @@
-#include <Arduino.h>
-#include "config.h"
-
 #ifndef status_handler
 #define status_handler
+
+#include <Arduino.h>
+#include "config.h"
 
 void blink(int, int = 1000, int = 500);
 
