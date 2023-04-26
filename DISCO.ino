68b5a990d18bbae596ec9454f40f658249ccd202
Dampi <dhermes.mail@web.de>
2023-04-26 13:33:26 +0200

    Added IMU library

--- DISCO.ino
+++ DISCO.ino
@@ -1,6 +1,8 @@
 #include <Timer.h>
 #include <SPI.h>  //Serial Peripheral Interface: Used for communication between SD card and Arduino
 #include <SD.h>
+#include <MPU6050_light.h>
+#include <Wire.h>
 
 #include "log_lib.h"
 #include "status_handler.h"
@@ -8,11 +10,15 @@
 
 Timer timer;      //global because needed in loop() and setup()
 String log_name;  //global because needed in loop() and setup()
+MPU6050 mpu(Wire);
+unsigned long saved_time = 0;
+
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  //For the LEDs
+
+
   //Starting Serial
-  
   Serial.begin(9600);  //Bit per second data transfer, for now we send only text which works good with 9600
   while (!Serial) {
     //this empty While is intentional, sometimes serial connection is not established immediately, but we need it so we wait...
@@ -21,19 +27,35 @@ void setup() {
   Serial.println(F("Serial started; May your Coffee kick in before the Rocket does..."));
 
 
+  Wire.begin(); //for the IMU
+  byte status = mpu.begin();
+  Serial.println("Inertial Mesuring Unit reports code: " + status);
+  while(status!=0) {}
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  mpu.calcOffsets();
+  delay(1000);
+  Serial.println(F("Done!"));
+
+
+
   SD_Connect();                  //connecting to SD card
   log_name = create_log("log");  //Creating
 
-
+  timer.start();
+  Serial.println("Setup finished.");
 }
 
 void loop() {
 
-  Serial.println("Start logging...");
-  record_event("status, Rocket Launched!", log_name, timer);
-  delay(5000);
-  record_event("gyro, -687.87", log_name, timer);
-  delay(3000);
-  record_event("acc, 23.6", log_name, timer);
+  mpu.update();
+  if((timer.read() - saved_time)>10){ // print data every 10ms
+    Serial.print("X : ");
+    Serial.print(mpu.getAngleX());
+    Serial.print("\tY : ");
+    Serial.print(mpu.getAngleY());
+    Serial.print("\tZ : ");
+    Serial.println(mpu.getAngleZ());
+    saved_time = timer.read();  
+  }
   
 }
