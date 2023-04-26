f972b89e4e92486ec4d85b97bbe7ac2ab79eb56e
ThePirate42 <git@thepirate42.org>
2023-04-26 23:35:57 +0200

    Formatting change

--- DISCO.ino
+++ DISCO.ino
@@ -15,34 +15,32 @@ unsigned long saved_time = 0;
 
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);  //For the LEDs
-
-
-  //Starting Serial
-  Serial.begin(9600);  //Bit per second data transfer, for now we send only text which works good with 9600
-  while (!Serial) {
-    //this empty While is intentional, sometimes serial connection is not established immediately, but we need it so we wait...
-    // why not a delay()? becouse we cant know exactly how long we have to wait, in this way we dont loose time.
-  }
-  Serial.println(F("Serial started; May your Coffee kick in before the Rocket does..."));
-
-
-  Wire.begin(); //for the IMU
-  byte status = mpu.begin();
-  Serial.println("Inertial Mesuring Unit reports code: " + status);
-  while(status!=0) {}
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
-  mpu.calcOffsets();
-  delay(1000);
-  Serial.println(F("Done!"));
-
-
-
-  SD_Connect();                  //connecting to SD card
-  log_name = create_log("log");  //Creating
-
-  timer.start();
-  Serial.println("Setup finished.");
+	
+	pinMode(LED_BUILTIN, OUTPUT);  //For the LEDs
+	
+	//Starting Serial
+	Serial.begin(9600);  //Bit per second data transfer, for now we send only text which works good with 9600
+	while (!Serial) {
+	//this empty While is intentional, sometimes serial connection is not established immediately, but we need it so we wait...
+	// why not a delay()? becouse we cant know exactly how long we have to wait, in this way we dont loose time.
+	}
+	Serial.println(F("Serial started; May your Coffee kick in before the Rocket does..."));
+		
+	Wire.begin(); //for the IMU
+	byte status = mpu.begin();
+	Serial.println("Inertial Mesuring Unit reports code: " + status);
+	while(status!=0) {}
+	Serial.println(F("Calculating offsets, do not move MPU6050"));
+	mpu.calcOffsets();
+	delay(1000);
+	Serial.println(F("Done!"));
+	
+	SD_Connect();                  //connecting to SD card
+	log_name = create_log("log");  //Creating
+	
+	timer.start();
+	Serial.println("Setup finished.");
+	
 }
 
 void loop() {
