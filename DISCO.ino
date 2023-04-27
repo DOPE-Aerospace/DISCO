fcc0d602c8fdfd381c04a04f8e2c40a242b8f872
Dampi <dhermes.mail@web.de>
2023-04-27 14:26:46 +0200

    Added new library.

--- DISCO.ino
+++ DISCO.ino
@@ -8,41 +8,51 @@
 #include "status_handler.h"
 #include "config.h"
 
-Timer timer;      //global because needed in loop() and setup()
-String log_name;  //global because needed in loop() and setup()
-MPU6050 mpu(Wire);
-unsigned long saved_time = 0;
+Timer timer;          //global because needed in loop() and setup()
+Logger logger("log", "time, data, content");      //global because needed in loop() and setup()
+MPU6050 mpu(Wire);    //Class for the IMU
+unsigned long saved_time = 0; //dynamic delay 
 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);  //For the LEDs
 
 
-  //Starting Serial
+  //=============
+  // SERIAL INIT
+  //=============
   Serial.begin(9600);  //Bit per second data transfer, for now we send only text which works good with 9600
   while (!Serial) {
-    //this empty While is intentional, sometimes serial connection is not established immediately, but we need it so we wait...
+    //this empty while is intentional, sometimes serial connection is not established immediately, but we need it so we wait...
     // why not a delay()? becouse we cant know exactly how long we have to wait, in this way we dont loose time.
   }
   Serial.println(F("Serial started; May your Coffee kick in before the Rocket does..."));
 
-
+  //===========
+  // IMU INIT
+  //===========
   Wire.begin(); //for the IMU
   byte status = mpu.begin();
   Serial.println("Inertial Mesuring Unit reports code: " + status);
-  while(status!=0) {}
+  if (status != 0) {
+    abort_blink(4);
+  }
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   mpu.calcOffsets();
   delay(1000);
   Serial.println(F("Done!"));
 
 
+  //=============
+  // Logger Init
+  //=============
+  //logger =
 
-  SD_Connect();                  //connecting to SD card
-  log_name = create_log("log");  //Creating
-
+  //===========
+  //   Misc
+  //===========
   timer.start();
-  Serial.println("Setup finished.");
+  Serial.println(F("Setup finished."));
 }
 
 void loop() {
