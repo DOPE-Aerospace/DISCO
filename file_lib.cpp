22ee739b0d8ad53fba1b409997a5845b865f09ee
ThePirate42 <git@thepirate42.org>
2023-05-03 22:28:37 +0200

    Added smart_file library

---
+++ file_lib.cpp
@@ -0,0 +1,69 @@
+/* POSSIBLE ERROR MESSAGES
+
+  SD Error -> two rapid blinks and three seconds idle...
+
+  ERROR_SD1: SD Card INIT Failed.
+
+*/
+
+#include <SPI.h>  //Serial Peripheral Interface: Used for communication between SD card and Arduino
+#include <SD.h>
+#include "status_handler.h"
+#include "file_lib.h"
+
+bool SD_initialized = false; //false at startup, it becomes true if initSD is called
+
+smart_file::smart_file(File&& file) : File(static_cast<File&&>(file)) {}
+
+smart_file::smart_file(const char *filename, uint8_t mode){
+	if(!SD_initialized){
+		initSD();
+	}
+	static_cast<File&>(*this) = SD.open(filename, mode);
+}
+
+smart_file::smart_file(const String &filename, uint8_t mode){
+	if(!SD_initialized){
+		initSD();
+	}
+	static_cast<File&>(*this) = SD.open(filename.c_str(), mode);
+}
+
+smart_file::~smart_file(){
+	if(*this){
+		close();
+	}
+}
+
+smart_file::operator bool(){
+	return static_cast<File>(*this);
+}
+
+smart_file smart_file::openNextFile(uint8_t mode){
+	return static_cast<File>(*this).openNextFile(mode);
+}
+
+void initSD(){
+
+	if (!SD.begin(SDCARD_SS_PIN)){  //CS is the Chip Select and could be interpreted like a Context Switch for peripherals, we use a Macro here.
+		Serial.println(F("ERROR_SD1: SD Card INIT Failed. is the card inserted?"));
+		abort_blink(2);
+	}
+	Serial.println(F("SD Card Recognized; The Gods are on our side..."));
+	SD_initialized = true;
+
+}
+
+bool file_exists(const char *filepath){
+	if(!SD_initialized){
+		initSD();
+	}
+	return SD.exists(filepath);
+}
+
+bool file_exists(const String &filepath){
+	if(!SD_initialized){
+		initSD();
+	}
+	return SD.exists(filepath.c_str());
+}
---
+++ file_lib.h
@@ -0,0 +1,47 @@
+#ifndef FILE_LIB
+#define FILE_LIB
+
+#include <SPI.h>  //Serial Peripheral Interface: Used for communication between SD card and Arduino
+#include <SD.h>
+
+void initSD(); //called by smart_file constructor if SD_initialized is false
+
+bool file_exists(const char *filepath);
+bool file_exists(const String &filepath);
+
+class smart_file : File {
+
+	smart_file(File&&);
+
+public:
+
+	smart_file() = default;
+	
+	smart_file(const char *filename, uint8_t mode = FILE_READ);
+	smart_file(const String &filename, uint8_t mode = FILE_READ);
+
+	~smart_file();
+
+	//using declarations to export member functions of File
+
+	using File::name;
+	using File::available;
+	using File::availableForWrite;
+	using File::close;
+	using File::flush;
+	using File::peek;
+	using File::position;
+	using File::print;
+	using File::println;
+	using File::seek;
+	using File::size;
+	using File::read;
+	using File::write;
+	using File::isDirectory;
+	using File::rewindDirectory;
+	explicit operator bool();
+	smart_file openNextFile(uint8_t mode = O_RDONLY);
+
+};
+
+#endif
